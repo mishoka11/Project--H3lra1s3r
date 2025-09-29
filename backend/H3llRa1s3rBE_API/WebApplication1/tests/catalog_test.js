ea32b0c0debe2a8c0d57b4a7ed88f63f25c924b4
mishoka11 <misho.mihov11@gmail.com>
2025-09-29 18:07:44 +0200

    Renamed backend folder and moved k6 tests

---
+++ backend/H3llRa1s3rBE_API/WebApplication1/tests/catalog_test.js
@@ -0,0 +1,18 @@
+﻿import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+    vus: 10,          // virtual users
+    duration: '30s',  // test length
+    thresholds: {
+        http_req_duration: ['p(95)<500'], // 95% of requests < 500ms
+    },
+};
+
+export default function () {
+    let res = http.get('http://localhost:8080/catalog');
+    check(res, {
+        'status is 200': (r) => r.status === 200,
+    });
+    sleep(1);
+}
